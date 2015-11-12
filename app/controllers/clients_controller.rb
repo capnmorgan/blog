a163e17aeec6fe796941f2fc49e69b28f0e6c981
capnmorgan <morgan@dealmaster.co>
2015-11-11 22:23:05 -0500

    no message

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,24 @@
 class ClientsController < ApplicationController
   def index
-    @clients = Client.all
+    if params[:filter]
+      if params[:filter] == "attempted"
+        @clients = Client.where(stage: "Attempted")
+      elsif params[:filter] == "sold"
+        @clients = Client.where(stage: "Sold")
+      elsif params[:filter] == "presenting"
+        @clients = Client.where(stage: "Presenting")
+      elsif params[:filter] == "appointment"
+        @clients = Client.where(stage: "Appointment")
+      elsif params[:filter] == "negotiating"
+        @clients = Client.where(stage: "Negotiating")
+      elsif params[:filter] == "notcontracted"
+        @clients = Client.where(stage: "Not Contracted")
+      elsif params[:filter] == "notinvolved"
+        @clients = Client.where(stage: "Not Involved")
+      end
+    else
+      @clients = Client.all
+    end
 
     #respond_to do |format|
     #  format.html do
